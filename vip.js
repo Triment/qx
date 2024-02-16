a98fe779e8320912ea5d0f109afc42e73ac95d45
as <as@asdeMac-Pro.lan>
2024-02-16 18:00:13 +0800

    使用data字段

--- vip.js
+++ vip.js
@@ -18,9 +18,9 @@ hostname = bzpp2.iwzbz.com
 *******************************/
 
 let obj = JSON.parse($response.body);
-obj.vipLevel = 3;
-obj.vipTipsType = 0;
-obj.expires = "2999-11-28\x2006:06:06";
+obj.data.vipLevel = 3;
+obj.data.vipTipsType = 0;
+obj.data.expires = "2999-11-28\x2006:06:06";
 let body = JSON.stringify(obj);
 console.log(body);
 $done(body);

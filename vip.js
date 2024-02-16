1d573fb1a4d4fc72dd400e8555eb31c028c1aee9
Triment <lsplub@gmail.com>
2024-02-16 23:57:04 +0800

    更新 vip.js

--- vip.js
+++ vip.js
@@ -19,7 +19,7 @@ hostname = bzpp2.iwzbz.com
 
 let obj = JSON.parse($response.body);
 obj.data.vipLevel = 3;
-obj.data.vipTipsType = 0;
+obj.data.vipTipsType = 1;
 obj.data.expires = "2999-11-28\x2006:06:06";
 obj.data.adtype = 0;
 let body = JSON.stringify(obj);
